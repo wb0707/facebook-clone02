b015edec066a16ac05f69baa5e9caa7a0b48b908
SR Shihab <84579722+wb0707@users.noreply.github.com>
2021-09-07 22:23:45 +0600

    changes

---
+++ index.js
@@ -0,0 +1,188 @@
+var postData = [
+    {
+        postProfilePic:'./member1.jpg',
+        namee:'MD Sojol',
+        groupName:'posted on Rong_Baz',
+        threeDot: 'fas fa-ellipsis-v',
+        postContent:'assalamualiku, আমি CSE তে পরতে চাই। আমি ইতিমধ্যেই HTML, CSS এবং basic python শিখেছি ।বর্তমানে C নিয়ে পড়াশোনা করছি। তামিম শাহ্‌রিয়ার সুবিন স্যার এর কম্পিউটার প্রোগ্রামিং ১ম খণ্ড প্রায় শেষ করে ফেলেছি অ্যান্ড online থেকে basic কিছু problem o solve  করেছি অ্যান্ড করছি।   আমি চাচ্ছিলাম ভর্তি হবার আগেই যতটা সম্ভব নিজেকে prepare করে নিতে।… See more ',
+        postImg:'./member3.jpg',
+        likeCount:'MD Shakil Ahmed, Md Sojib Ahmed and 35 others',
+        commentCount:'36 Comments'               
+        
+    },
+    {
+        postProfilePic:'./member2.jpg',
+        namee:'MD Karim',
+        groupName:'posted on Rong_Baz',
+        threeDot: 'fas fa-ellipsis-v',
+        postContent:'assalamualiku, আমি CSE তে পরতে চাই। আমি ইতিমধ্যেই HTML, CSS এবং basic python শিখেছি ।বর্তমানে C নিয়ে পড়াশোনা করছি। তামিম শাহ্‌রিয়ার সুবিন স্যার এর কম্পিউটার প্রোগ্রামিং ১ম খণ্ড প্রায় শেষ করে ফেলেছি অ্যান্ড online থেকে basic কিছু problem o solve  করেছি অ্যান্ড করছি।   আমি চাচ্ছিলাম ভর্তি হবার আগেই যতটা সম্ভব নিজেকে prepare করে নিতে।… See more ',
+        postImg:'./member5.jpg',
+        likeCount:'MD Shakil Ahmed, Md Sojib Ahmed and 39 others',
+        commentCount:'38 Comments'               
+        
+    },
+    {
+        postProfilePic:'./member7.jpg',
+        namee:'MD Sojol',
+        groupName:'posted on Rong_Baz',
+        threeDot: 'fas fa-ellipsis-v',
+        postContent:'assalamualiku, আমি CSE তে পরতে চাই। আমি ইতিমধ্যেই HTML, CSS এবং basic python শিখেছি ।বর্তমানে C নিয়ে পড়াশোনা করছি। তামিম শাহ্‌রিয়ার সুবিন স্যার এর কম্পিউটার প্রোগ্রামিং ১ম খণ্ড প্রায় শেষ করে ফেলেছি অ্যান্ড online থেকে basic কিছু problem o solve  করেছি অ্যান্ড করছি।   আমি চাচ্ছিলাম ভর্তি হবার আগেই যতটা সম্ভব নিজেকে prepare করে নিতে।… See more ',
+        postImg:'./member7.jpg',
+        likeCount:'MD Shakil Ahmed, Md Sojib Ahmed and 35 others',
+        commentCount:'36 Comments'               
+        
+    },
+    {
+        postProfilePic:'./member3.jpg',
+        namee:'Sojib aly',
+        groupName:'posted on Rong_Baz',
+        threeDot: 'fas fa-ellipsis-v',
+        postContent:'assalamualiku, আমি CSE তে পরতে চাই। আমি ইতিমধ্যেই HTML, CSS এবং basic python শিখেছি ।বর্তমানে C নিয়ে পড়াশোনা করছি। তামিম শাহ্‌রিয়ার সুবিন স্যার এর কম্পিউটার প্রোগ্রামিং ১ম খণ্ড প্রায় শেষ করে ফেলেছি অ্যান্ড online থেকে basic কিছু problem o solve  করেছি অ্যান্ড করছি।   আমি চাচ্ছিলাম ভর্তি হবার আগেই যতটা সম্ভব নিজেকে prepare করে নিতে।… See more ',
+        postImg:'./member6.jpg',
+        likeCount:'MD Shakil Ahmed, Md Sojib Ahmed and 45 others',
+        commentCount:'56 Comments'               
+        
+    },
+    {
+        postProfilePic:'./member1.jpg',
+        namee:'MD Sojol',
+        groupName:'posted on Rong_Baz',
+        threeDot: 'fas fa-ellipsis-v',
+        postContent:'assalamualiku, আমি CSE তে পরতে চাই। আমি ইতিমধ্যেই HTML, CSS এবং basic python শিখেছি ।বর্তমানে C নিয়ে পড়াশোনা করছি। তামিম শাহ্‌রিয়ার সুবিন স্যার এর কম্পিউটার প্রোগ্রামিং ১ম খণ্ড প্রায় শেষ করে ফেলেছি অ্যান্ড online থেকে basic কিছু problem o solve  করেছি অ্যান্ড করছি।   আমি চাচ্ছিলাম ভর্তি হবার আগেই যতটা সম্ভব নিজেকে prepare করে নিতে।… See more ',
+        postImg:'./member3.jpg',
+        likeCount:'MD Shakil Ahmed, Md Sojib Ahmed and 35 others',
+        commentCount:'36 Comments'               
+        
+    },
+    {
+        postProfilePic:'./member2.jpg',
+        namee:'MD Karim',
+        groupName:'posted on Rong_Baz',
+        threeDot: 'fas fa-ellipsis-v',
+        postContent:'assalamualiku, আমি CSE তে পরতে চাই। আমি ইতিমধ্যেই HTML, CSS এবং basic python শিখেছি ।বর্তমানে C নিয়ে পড়াশোনা করছি। তামিম শাহ্‌রিয়ার সুবিন স্যার এর কম্পিউটার প্রোগ্রামিং ১ম খণ্ড প্রায় শেষ করে ফেলেছি অ্যান্ড online থেকে basic কিছু problem o solve  করেছি অ্যান্ড করছি।   আমি চাচ্ছিলাম ভর্তি হবার আগেই যতটা সম্ভব নিজেকে prepare করে নিতে।… See more ',
+        postImg:'./member5.jpg',
+        likeCount:'MD Shakil Ahmed, Md Sojib Ahmed and 39 others',
+        commentCount:'38 Comments'               
+        
+    },
+    {
+        postProfilePic:'./member7.jpg',
+        namee:'MD Sojol',
+        groupName:'posted on Rong_Baz',
+        threeDot: 'fas fa-ellipsis-v',
+        postContent:'assalamualiku, আমি CSE তে পরতে চাই। আমি ইতিমধ্যেই HTML, CSS এবং basic python শিখেছি ।বর্তমানে C নিয়ে পড়াশোনা করছি। তামিম শাহ্‌রিয়ার সুবিন স্যার এর কম্পিউটার প্রোগ্রামিং ১ম খণ্ড প্রায় শেষ করে ফেলেছি অ্যান্ড online থেকে basic কিছু problem o solve  করেছি অ্যান্ড করছি।   আমি চাচ্ছিলাম ভর্তি হবার আগেই যতটা সম্ভব নিজেকে prepare করে নিতে।… See more ',
+        postImg:'./member7.jpg',
+        likeCount:'MD Shakil Ahmed, Md Sojib Ahmed and 35 others',
+        commentCount:'36 Comments'               
+        
+    },
+    {
+        postProfilePic:'./member3.jpg',
+        namee:'Sojib aly',
+        groupName:'posted on Rong_Baz',
+        threeDot: 'fas fa-ellipsis-v',
+        postContent:'assalamualiku, আমি CSE তে পরতে চাই। আমি ইতিমধ্যেই HTML, CSS এবং basic python শিখেছি ।বর্তমানে C নিয়ে পড়াশোনা করছি। তামিম শাহ্‌রিয়ার সুবিন স্যার এর কম্পিউটার প্রোগ্রামিং ১ম খণ্ড প্রায় শেষ করে ফেলেছি অ্যান্ড online থেকে basic কিছু problem o solve  করেছি অ্যান্ড করছি।   আমি চাচ্ছিলাম ভর্তি হবার আগেই যতটা সম্ভব নিজেকে prepare করে নিতে।… See more ',
+        postImg:'./member6.jpg',
+        likeCount:'MD Shakil Ahmed, Md Sojib Ahmed and 45 others',
+        commentCount:'56 Comments'               
+        
+    },
+    {
+        postProfilePic:'./member1.jpg',
+        namee:'MD Sojol',
+        groupName:'posted on Rong_Baz',
+        threeDot:'fas fa-ellipsis-v',
+        postContent:'assalamualiku, আমি CSE তে পরতে চাই। আমি ইতিমধ্যেই HTML, CSS এবং basic python শিখেছি ।বর্তমানে C নিয়ে পড়াশোনা করছি। তামিম শাহ্‌রিয়ার সুবিন স্যার এর কম্পিউটার প্রোগ্রামিং ১ম খণ্ড প্রায় শেষ করে ফেলেছি অ্যান্ড online থেকে basic কিছু problem o solve  করেছি অ্যান্ড করছি।   আমি চাচ্ছিলাম ভর্তি হবার আগেই যতটা সম্ভব নিজেকে prepare করে নিতে।… See more ',
+        postImg:'./member3.jpg',
+        likeCount:'MD Shakil Ahmed, Md Sojib Ahmed and 35 others',
+        commentCount:'36 Comments'               
+        
+    },
+    {
+        postProfilePic:'./member2.jpg',
+        namee:'MD Karim',
+        groupName:'posted on Rong_Baz',
+        threeDot:':',
+        postContent:'assalamualiku, আমি CSE তে পরতে চাই। আমি ইতিমধ্যেই HTML, CSS এবং basic python শিখেছি ।বর্তমানে C নিয়ে পড়াশোনা করছি। তামিম শাহ্‌রিয়ার সুবিন স্যার এর কম্পিউটার প্রোগ্রামিং ১ম খণ্ড প্রায় শেষ করে ফেলেছি অ্যান্ড online থেকে basic কিছু problem o solve  করেছি অ্যান্ড করছি।   আমি চাচ্ছিলাম ভর্তি হবার আগেই যতটা সম্ভব নিজেকে prepare করে নিতে।… See more ',
+        postImg:'./member5.jpg',
+        likeCount:'MD Shakil Ahmed, Md Sojib Ahmed and 39 others',
+        commentCount:'38 Comments'               
+        
+    },
+    {
+        postProfilePic:'./member7.jpg',
+        namee:'MD Sojol',
+        groupName:'posted on Rong_Baz',
+        threeDot:':',
+        postContent:'assalamualiku, আমি CSE তে পরতে চাই। আমি ইতিমধ্যেই HTML, CSS এবং basic python শিখেছি ।বর্তমানে C নিয়ে পড়াশোনা করছি। তামিম শাহ্‌রিয়ার সুবিন স্যার এর কম্পিউটার প্রোগ্রামিং ১ম খণ্ড প্রায় শেষ করে ফেলেছি অ্যান্ড online থেকে basic কিছু problem o solve  করেছি অ্যান্ড করছি।   আমি চাচ্ছিলাম ভর্তি হবার আগেই যতটা সম্ভব নিজেকে prepare করে নিতে।… See more ',
+        postImg:'./member7.jpg',
+        likeCount:'MD Shakil Ahmed, Md Sojib Ahmed and 35 others',
+        commentCount:'36 Comments'               
+        
+    },
+    {
+        postProfilePic:'./member3.jpg',
+        namee:'Sojib aly',
+        groupName:'posted on Rong_Baz',
+        threeDot: 'fas fa-ellipsis-v',
+        postContent:'assalamualiku, আমি CSE তে পরতে চাই। আমি ইতিমধ্যেই HTML, CSS এবং basic python শিখেছি ।বর্তমানে C নিয়ে পড়াশোনা করছি। তামিম শাহ্‌রিয়ার সুবিন স্যার এর কম্পিউটার প্রোগ্রামিং ১ম খণ্ড প্রায় শেষ করে ফেলেছি অ্যান্ড online থেকে basic কিছু problem o solve  করেছি অ্যান্ড করছি।   আমি চাচ্ছিলাম ভর্তি হবার আগেই যতটা সম্ভব নিজেকে prepare করে নিতে।… See more ',
+        postImg:'./member6.jpg',
+        likeCount:'MD Shakil Ahmed, Md Sojib Ahmed and 45 others',
+        commentCount:'56 Comments'               
+        
+    }
+];
+
+postData.forEach(postFunc);
+
+function postFunc(item){
+ 
+var postParent = document.getElementById('post-main');
+var {postProfilePic,namee,groupName,threeDot,postContent,postImg,likeCount,commentCount} = item;
+postParent.innerHTML += 
+                    `  <div class="post">
+                        <div class="post-title">
+                            <img class="post-profile-pic" src="${postProfilePic}" alt="profile-pic">
+                            <h3>${namee}</h3>
+                            <p>${groupName}</p>
+                            <p class="three-dot"> <i class="${threeDot}"></i></p>
+                        </div>
+                        <div class="post-content">
+                            ${postContent}
+                        </div>
+                        <div class="post-img">
+                            <img src="${postImg}" alt="post-img">
+                        </div>
+                        <div class="like-cmnt-count">
+                            <span>${likeCount} </span>
+                            <span> ${commentCount}</span>
+                        </div>
+                        <div class="like-comment">
+                            <button>
+                              <span><i class="far fa-thumbs-up"></i> Like </span>
+                            </button>
+                            <button>
+                            <span><i class="far fa-comments"></i> Comment</span>
+                            </button>
+                            <button>
+                            <span><i class="fas fa-share"></i> Share </span>
+                            </button>
+                        </div>
+
+                </div>
+             `
+;
+
+}
+
+
+
+// const setTimeout(()=>{
+  
+// alert('This website have no functionallity, it is a just simple Frontend  design...');
+// },1000);
+
+setTimeout(function(){
+     alert('This website have no functionallity, it is a just simple Frontend  design...');
+
+}, 2000);
